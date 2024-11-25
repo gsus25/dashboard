80d3e45dd47128d092436f5788be99d8f5e00b0e
Jesus Suarez <jedasuar@espol.edu.ec>
2024-11-25 16:04:45 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -13,26 +13,26 @@ function App() {
     <Grid container spacing={5}>
 
         {/* Indicadores */}
-        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} /></Grid>
-        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"}/></Grid>
-        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"}/></Grid>
-        <Grid size={{ xs: 12, xl: 3 }}><IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"}/></Grid>
+        <Grid size={{ xs: 12, md: 3 }}><IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} /></Grid>
+        <Grid size={{ xs: 12, md: 3 }}><IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"}/></Grid>
+        <Grid size={{ xs: 12, md: 3 }}><IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"}/></Grid>
+        <Grid size={{ xs: 12, md: 3 }}><IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"}/></Grid>
 
         {/* Tabla */}
-        <Grid size={{ xs: 12, xl: 8 }}>
+        <Grid size={{ xs: 12, md: 8 }}>
           {/* Grid Anidado */}
           <Grid container spacing={2}>
-            <Grid size={{ xs: 12, xl: 3 }}>
+            <Grid size={{ xs: 12, md: 3 }}>
               <ControlWeather/>
             </Grid>
-            <Grid size={{ xs: 12, xl: 9 }}>
+            <Grid size={{ xs: 12, md: 9 }}>
               <TableWeather/>
             </Grid>
           </Grid>
         </Grid>
 
         {/* Gráfico */}
-        <Grid size={{ xs: 12, xl: 4 }}>
+        <Grid size={{ xs: 12, md: 4 }}>
           <LineChartWeather/>
         </Grid>
        
