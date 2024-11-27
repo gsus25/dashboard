4d27088f10ca01c3b7e8ce1a9ec94e065f98e227
Jesus Suarez <jedasuar@espol.edu.ec>
2024-11-27 14:09:55 -0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,60 @@ import IndicatorWeather from './components/IndicatorWeather';
 import TableWeather from './components/TableWeather';
 import ControlWeather from './components/ControlWeather';
 import LineChartWeather from './components/LineChartWeather';
+{/* Hooks */ }
+import { useEffect } from 'react';
+
+interface Indicator {
+  title?: String;
+  subtitle?: String;
+  value?: String;
+}
 
 function App() {
+
+  {/* Hook: useEffect */}
+  useEffect( ()=>{
+    let request = async () => { 
+
+      {/* Request */}
+      let API_KEY = "OPENWEATHERMAP' API KEY"
+      let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Guayaquil&mode=xml&appid=${API_KEY}`)
+      let savedTextXML = await response.text();
+
+      {/* XML Parser */}
+      const parser = new DOMParser();
+      const xml = parser.parseFromString(savedTextXML, "application/xml");
+
+      {/* Arreglo para agregar los resultados */}
+
+      let dataToIndicators : Indicator[] = new Array<Indicator>();
+
+      {/* 
+          Análisis, extracción y almacenamiento del contenido del XML 
+          en el arreglo de resultados
+      */}
+
+      let name = xml.getElementsByTagName("name")[0].innerHTML || ""
+      dataToIndicators.push({"title":"Location", "subtitle": "City", "value": name})
+
+      let location = xml.getElementsByTagName("location")[1]
+
+      let latitude = location.getAttribute("latitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Latitude", "value": latitude })
+
+      let longitude = location.getAttribute("longitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Longitude", "value": longitude })
+
+      let altitude = location.getAttribute("altitude") || ""
+      dataToIndicators.push({ "title": "Location", "subtitle": "Altitude", "value": altitude })
+
+      console.log( dataToIndicators )
+
+    }
+
+        request();
+
+  }, [] )
 
   return (
     <Grid container spacing={5}>
