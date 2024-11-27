fe65c9140a9de6192d72bf1109d3b1a478a40e1f
Jesus Suarez <jedasuar@espol.edu.ec>
2024-11-27 14:30:38 -0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import TableWeather from './components/TableWeather';
 import ControlWeather from './components/ControlWeather';
 import LineChartWeather from './components/LineChartWeather';
 {/* Hooks */ }
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 interface Indicator {
   title?: String;
@@ -16,6 +16,9 @@ interface Indicator {
 }
 
 function App() {
+
+  {/* Variable de estado y función de actualización */}
+  let [indicators, setIndicators] = useState<Indicator[]>([])
 
   {/* Hook: useEffect */}
   useEffect( ()=>{
@@ -54,21 +57,38 @@ function App() {
       dataToIndicators.push({ "title": "Location", "subtitle": "Altitude", "value": altitude })
 
       console.log( dataToIndicators )
+      {/* Modificación de la variable de estado mediante la función de actualización */}
+      setIndicators( dataToIndicators )
 
     }
 
         request();
 
   }, [] )
+  let renderIndicators = () => {
+
+    return indicators
+            .map(
+                (indicator, idx) => (
+                    <Grid key={idx} size={{ xs: 12, xl: 3 }}>
+                        <IndicatorWeather 
+                            title={indicator["title"]} 
+                            subtitle={indicator["subtitle"]} 
+                            value={indicator["value"]} />
+                    </Grid>
+                )
+            )
+     
+  } 
 
   return (
     <Grid container spacing={5}>
 
-        {/* Indicadores */}
+        {/* Indicadores
         <Grid size={{ xs: 12, md: 3 }}><IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} /></Grid>
         <Grid size={{ xs: 12, md: 3 }}><IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"}/></Grid>
         <Grid size={{ xs: 12, md: 3 }}><IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"}/></Grid>
-        <Grid size={{ xs: 12, md: 3 }}><IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"}/></Grid>
+        <Grid size={{ xs: 12, md: 3 }}><IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"}/></Grid> */}
 
         {/* Tabla */}
         <Grid size={{ xs: 12, md: 8 }}>
@@ -87,6 +107,8 @@ function App() {
         <Grid size={{ xs: 12, md: 4 }}>
           <LineChartWeather/>
         </Grid>
+
+        {renderIndicators()}
        
     </Grid>
 )
