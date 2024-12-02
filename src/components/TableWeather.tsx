ab3f839e21bdedad9597408730098d4ab75d7306
Jesus Suarez <jedasuar@espol.edu.ec>
2024-12-02 14:17:21 -0500

    update

--- src/components/TableWeather.tsx
+++ src/components/TableWeather.tsx
@@ -25,11 +25,11 @@ export default function BasicTable(props: MyProp) {
         {/* Cabeceras de la tabla */}
         <TableHead>
           <TableRow>
-            <TableCell>Hora de inicio</TableCell>
-            <TableCell align="right">Hora de fin</TableCell>
-            <TableCell align="right">Precipitación</TableCell>
-            <TableCell align="right">Humedad</TableCell>
-            <TableCell align="right">Nubosidad</TableCell>
+            <TableCell align="center">Hora de inicio</TableCell>
+            <TableCell align="center">Hora de fin</TableCell>
+            <TableCell align="center">Precipitación</TableCell>
+            <TableCell align="center">Humedad</TableCell>
+            <TableCell align="center">Nubosidad</TableCell>
           </TableRow>
         </TableHead>
         {/* Cuerpo de la tabla */}
@@ -39,13 +39,13 @@ export default function BasicTable(props: MyProp) {
               key={idx}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
+              <TableCell component="th" scope="row" align="center">
                 {row.dateStart}
               </TableCell>
-              <TableCell align="right">{row.dateEnd}</TableCell>
-              <TableCell align="right">{row.precipitation}</TableCell>
-              <TableCell align="right">{row.humidity}</TableCell>
-              <TableCell align="right">{row.clouds}</TableCell>
+              <TableCell align="center">{row.dateEnd}</TableCell>
+              <TableCell align="center">{row.precipitation}</TableCell>
+              <TableCell align="center">{row.humidity}</TableCell>
+              <TableCell align="center">{row.clouds}</TableCell>
             </TableRow>
           ))}
         </TableBody>
